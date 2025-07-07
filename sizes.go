ca0e0c6405543ac6ecc52834dacfd69b2fadd509
Andrew Brown <ab36245@gmail.com>
2025-07-07 17:03:47 +1000

    Brought in line with dart-mspack

--- sizes.go
+++ sizes.go
@@ -1,23 +1,36 @@
 package msgpack
 
 const (
-	mask4  = 0x0f
-	mask5  = 0x1f
-	mask6  = 0x3f
-	mask7  = 0x7f
-	mask8  = 0xff
+	mask4 = 0x0f
+	mask5 = 0x1f
+	mask6 = 0x3f
+
+	mask8 = 0xff
+	size8 = mask8 + 1
+	mask7 = mask8 >> 1
+
 	mask16 = 0xffff
+	size16 = mask16 + 1
+	mask15 = mask16 >> 1
+
 	mask32 = 0xffffffff
+	size32 = mask32 + 1
+	mask31 = mask32 >> 1
+
 	mask34 = 0x03ffffffff
+	size34 = mask34 + 1
 )
 
 const (
-	intFixMin = -(mask6 >> 1) - 1
-	intFixMax = mask8 >> 1
-	int8Min   = -(mask8 >> 1) - 1
-	int8Max   = mask8 >> 1
-	int16Min  = -(mask16 >> 1) - 1
-	int16Max  = mask16 >> 1
-	int32Min  = -(mask32 >> 1) - 1
-	int32Max  = mask32 >> 1
+	intFixMax = mask7
+	intFixMin = -32
+
+	int8Max = mask7
+	int8Min = -int8Max - 1
+
+	int16Max = mask15
+	int16Min = -int16Max - 1
+
+	int32Max = mask31
+	int32Min = -int32Max - 1
 )
