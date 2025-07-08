e6ceb0977ee2f3a821490847137cf8773d3fa4e8
Andrew Brown <ab36245@gmail.com>
2025-07-08 11:12:55 +1000

    Added to float tests

--- test/float_test.go
+++ test/float_test.go
@@ -6,34 +6,6 @@ import (
 	"github.com/ab36245/go-msgpack"
 )
 
-func TestFloat(t *testing.T) {
-	run := func(t *testing.T, f float64, e string) {
-		mpe := msgpack.NewEncoder()
-		mpe.PutFloat(f)
-		mps := mpe.AsString(-1)
-		if mps != e {
-			report(t, mps, e)
-		}
-		mpd := msgpack.NewDecoder(mpe.Bytes())
-		a, _ := mpd.GetFloat()
-		if a != f {
-			report(t, a, f)
-		}
-	}
-
-	t.Run("85.125", func(t *testing.T) {
-		run(t, 85.125, "ca 42 aa 40 00")
-	})
-
-	t.Run("85.3", func(t *testing.T) {
-		run(t, 85.3, "cb 40 55 53 33 33 33 33 33")
-	})
-
-	t.Run("0.00085125", func(t *testing.T) {
-		run(t, 0.00085125, "cb 3f 4b e4 cd 74 92 79 14")
-	})
-}
-
 func TestFloat32(t *testing.T) {
 	run := func(t *testing.T, f float32, e string) {
 		mpe := msgpack.NewEncoder()
@@ -59,6 +31,10 @@ func TestFloat32(t *testing.T) {
 
 	t.Run("0.00085125", func(t *testing.T) {
 		run(t, 0.00085125, "ca 3a 5f 26 6c")
+	})
+
+	t.Run("3.1415", func(t *testing.T) {
+		run(t, 3.1415, "ca 40 49 0e 56")
 	})
 }
 
@@ -87,5 +63,41 @@ func TestFloat64(t *testing.T) {
 
 	t.Run("0.00085125", func(t *testing.T) {
 		run(t, 0.00085125, "cb 3f 4b e4 cd 74 92 79 14")
+	})
+
+	t.Run("3.1415", func(t *testing.T) {
+		run(t, 3.1415, "cb 40 09 21 ca c0 83 12 6f")
+	})
+}
+
+func TestFloat(t *testing.T) {
+	run := func(t *testing.T, f float64, e string) {
+		mpe := msgpack.NewEncoder()
+		mpe.PutFloat(f)
+		mps := mpe.AsString(-1)
+		if mps != e {
+			report(t, mps, e)
+		}
+		mpd := msgpack.NewDecoder(mpe.Bytes())
+		a, _ := mpd.GetFloat()
+		if a != f {
+			report(t, a, f)
+		}
+	}
+
+	t.Run("85.125", func(t *testing.T) {
+		run(t, 85.125, "ca 42 aa 40 00")
+	})
+
+	t.Run("85.3", func(t *testing.T) {
+		run(t, 85.3, "cb 40 55 53 33 33 33 33 33")
+	})
+
+	t.Run("0.00085125", func(t *testing.T) {
+		run(t, 0.00085125, "cb 3f 4b e4 cd 74 92 79 14")
+	})
+
+	t.Run("3.1415", func(t *testing.T) {
+		run(t, 3.1415, "ca 40 49 0e 56")
 	})
 }
